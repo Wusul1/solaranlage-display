02c7e5a44225f4e4daa255cf900277dadcdd1781
Wusul1 <96010540+Wusul1@users.noreply.github.com>
2024-08-14 01:13:40 +0200

    Added Server

---
+++ dataserver.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+datastring = "0;0;0;0"
+
+@app.route("/senddata", methods=['POST'])
+def senddata():
+    global datastring
+    datastring = request.data.decode("utf-8")
+    return "ok"
+
+@app.route("/getdata")
+def getdata():
+	global datastring
+	return datastring
+	
+app.run()
