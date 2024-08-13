9638354520c07611ac1da0ec85647d3c12e9e96b
Wusul1 <96010540+Wusul1@users.noreply.github.com>
2024-08-13 23:19:21 +0200

    Add script to redirect to dashboard

---
+++ sunnyportaldashboardredirect.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        sunnyportal.com Dashboard Redirect
+// @namespace   Violentmonkey Scripts
+// @match       https://www.sunnyportal.com/FixedPages/HoManLive.aspx*
+// @grant       none
+// @run-at      document-start
+// @version     1.0
+// @author      -
+// @description Redirect from https://www.sunnyportal.com/FixedPages/HoManLive.aspx to https://www.sunnyportal.com/FixedPages/Dashboard.aspx
+// ==/UserScript==
+(function(){
+  window.location.replace("https://www.sunnyportal.com/FixedPages/Dashboard.aspx")
+})()
