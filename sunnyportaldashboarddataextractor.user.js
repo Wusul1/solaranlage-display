265b17d83017f5003bfc4a602fc3858c9096ecc6
Wusul1 <96010540+Wusul1@users.noreply.github.com>
2024-08-14 00:29:13 +0200

    Added Data Extraction

---
+++ sunnyportaldashboarddataextractor.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        sunnyportal.com Dashboard Data Extractor
+// @namespace   Violentmonkey Scripts
+// @match       https://www.sunnyportal.com/FixedPages/Dashboard.aspx*
+// @grant       none
+// @run-at      document-end
+// @version     1.0
+// @author      -
+// @description Extracts Data from Sunnyportal Dashboard and sends them to a Web Server
+// ==/UserScript==
+
+(function(){
+  var numberOfTimesDataWasExtracted = 0;
+  batteryChargeLeftPercentElement = document.getElementsByClassName("batterySoc");
+  if(batteryChargeLeftPercentElement.length == 0){
+    return;
+  }
+  batteryChargeLeftPercentElement = batteryChargeLeftPercentElement[0];
+  valueElements = document.getElementsByClassName("mainValueAmount");
+  if(valueElements.length<3){
+    return;
+  }
+  pvPowerWattageElement = valueElements[0];
+  currentConsumptionElement = valueElements[1];
+  batteryDischargeWattageElement = valueElements[2];
+
+  setInterval(() => {
+    numberOfTimesDataWasExtracted+=1
+    let pvPowerWatts = pvPowerWattageElement.textContent;
+    let powerConsumptionWatts = currentConsumptionElement.textContent
+    let batteryDischargeWatts = batteryDischargeWattageElement.textContent
+    let batteryChargeLeftPercent = batteryChargeLeftPercentElement.textContent.slice(0,-2)//remove " %" at the end of text
+  }, 1000);
+})();
